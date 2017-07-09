b81576986f308f66665dfc1909fd62b1d9ead13e
Jason DeBolt <jasondebolt@gmail.com>
2017-07-08 17:50:09 -0700

    basic flask app works both locally and on AWS lambda

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # veganoloia
+
+# Running the Flask local dev server
+```
+$ export FLASK_APP=app
+$ flask run
+```
+
+# Deploying the dev server to AWS Lambda
+```
+$ zappa deploy dev
+$ zappa update dev
+```
+
+# Setting up the JS frontend
+```
+https://realpython.com/blog/python/the-ultimate-flask-front-end/
+```
--- app.py
+++ app.py
@@ -14,15 +14,18 @@ secrets_dict = helpers.parse_json_config(
     os.path.join(app.root_path, 'veganolia_config.json'))
 
 # Load default config and override config from an environment variable
+
 app.config.update(dict(
     SECRET_KEY=secrets_dict['secret_key'],
     USERNAME=secrets_dict['username'],
-    PASSWORD=secrets_dict['password']
+    PASSWORD=secrets_dict['password'],
+    GOOGLE_ID=secrets_dict['google_id'],
+    GOOGLE_SECRET=secrets_dict['google_secret']
 ))
 
 @app.route('/')
 def index():
-    return render_template('index.html', entries=items)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     # The following code only gets executed when the app is run locally
